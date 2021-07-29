5981450acfb24615e62a41f6b0266e45b7dc89d0
Chris Lewis <drchrislewis@gmail.com>
2021-07-29 10:28:30 -0500

    clang, removed unnecessary and complex if(), renamed a static function in half-edge code.

--- tool_path_planner/include/tool_path_planner/utilities.h
+++ tool_path_planner/include/tool_path_planner/utilities.h
@@ -135,23 +135,27 @@ ToolPaths reverseOddRasters(const ToolPaths& tool_paths, RasterStyle raster_styl
 
 double computeOffsetSign(const ToolPathSegment& adjusted_segment, const ToolPathSegment& away_from_segment);
 
-pcl::PointCloud<pcl::PointNormal> computeMLSMeshNormals(const pcl::PointCloud<pcl::PointXYZ>::Ptr& mesh_cloud, double normal_search_radius);
-  
+pcl::PointCloud<pcl::PointNormal> computeMLSMeshNormals(const pcl::PointCloud<pcl::PointXYZ>::Ptr& mesh_cloud,
+                                                        double normal_search_radius);
+
 bool applyEqualDistance(const std::vector<int>& pnt_indices,
-			const pcl::PointCloud<pcl::PointXYZ>& mesh_cloud,
+                        const pcl::PointCloud<pcl::PointXYZ>& mesh_cloud,
                         pcl::PointCloud<pcl::PointNormal>& path_cloud,
                         double dist);
 
 bool insertPointNormals(const pcl::PointCloud<pcl::PointNormal>::Ptr& mesh_cloud,
-			pcl::PointCloud<pcl::PointNormal>& path_cloud);
+                        pcl::PointCloud<pcl::PointNormal>& path_cloud);
 
-void shapeMsgToPclPointXYZ( const shape_msgs::Mesh& mesh, pcl::PointCloud<pcl::PointXYZ>& mesh_cloud);
-
+void shapeMsgToPclPointXYZ(const shape_msgs::Mesh& mesh, pcl::PointCloud<pcl::PointXYZ>& mesh_cloud);
 
 void computeFaceNormals(const shape_msgs::Mesh& mesh, std::vector<Eigen::Vector3d>& face_normals);
 
-void computeMeshNormals(const shape_msgs::Mesh& mesh, std::vector<Eigen::Vector3d>& face_normals, std::vector<Eigen::Vector3d>& vertex_normals);
-void computePCLMeshNormals(pcl::PolygonMesh::ConstPtr& mesh, std::vector<Eigen::Vector3d>& face_normals, std::vector<Eigen::Vector3d>& vertex_normals);
+void computeMeshNormals(const shape_msgs::Mesh& mesh,
+                        std::vector<Eigen::Vector3d>& face_normals,
+                        std::vector<Eigen::Vector3d>& vertex_normals);
+void computePCLMeshNormals(pcl::PolygonMesh::ConstPtr& mesh,
+                           std::vector<Eigen::Vector3d>& face_normals,
+                           std::vector<Eigen::Vector3d>& vertex_normals);
 
 bool alignToVertexNormals(pcl::PointCloud<pcl::PointNormal>& mesh_cloud, std::vector<Eigen::Vector3d>& vertex_normals);
 }  // namespace tool_path_planner
